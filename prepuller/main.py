317b86fb2c78dbb705159925eb2915745141d7df
adam <athornton@gmail.com>
2018-02-14 17:47:29 -0700

    Getting repo images now.

--- prepuller/main.py
+++ prepuller/main.py
@@ -1,20 +1,49 @@
 #!/usr/bin/env python3
-from kubernetes import client, config
-from kubernetes.config.config_exception import ConfigException
+import argparse
+from .prepuller import Prepuller
 
 
 def standalone():
-    try:
-        config.load_incluster_config()
-    except ConfigException:
-        config.load_kube_config()
-    v1 = client.CoreV1Api()
-    print("Listing pods with their IPs:")
-    ret = v1.list_pod_for_all_namespaces(watch=False)
-    for i in ret.items:
-        print("%s\t%s\t%s" %
-              (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+    args = parse_args()
+    prepuller = Prepuller(args=args)
+    prepuller.repo.scan()
 
+
+def parse_args():
+    """Parse command-line arguments"""
+    desc = "Scan Docker repo for prepull images."
+    parser = argparse.ArgumentParser(description=desc)
+    parser.add_argument("-d", "--debug", action="store_true",
+                        help="enable debugging")
+    parser.add_argument("-r", "--repo", "--repository",
+                        help="repository host [hub.docker.com]",
+                        default="hub.docker.com")
+    parser.add_argument("-o", "--owner", "--organization", "--org",
+                        help="repository owner [lsstsqre]",
+                        default="lsstsqre")
+    parser.add_argument("-n", "--name",
+                        help="repository name [jld-lab]",
+                        default="jld-lab")
+    parser.add_argument("-q", "--dailies", "--daily", "--quotidian", type=int,
+                        help="# of daily builds to keep [3]",
+                        default=3)
+    parser.add_argument("-w", "--weeklies", "--weekly", type=int,
+                        help="# of weekly builds to keep [2]",
+                        default=2)
+    parser.add_argument("-b", "--releases", "--release", type=int,
+                        help="# of release builds to keep [1]",
+                        default=1)
+    parser.add_argument("-i", "--insecure", "--no-tls", "--no-ssl",
+                        help="Do not use TLS to connect [False]",
+                        type=bool,
+                        default=False)
+    parser.add_argument("-s", "--sort", "--sort-field", "--sort-by",
+                        help="Field to sort results by [comp_ts]",
+                        default="comp_ts")
+    results = parser.parse_args()
+    results.path = ("/v2/repositories/" + results.owner + "/" +
+                    results.name + "/tags")
+    return results
 
 if __name__ == "__main__":
     standalone()
